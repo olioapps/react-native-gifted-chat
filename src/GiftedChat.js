632bb80407ed30deb1f1084adc758419ff9f0b75
Jason Lane <quizathaderat@yahoo.com>
2016-09-01 18:03:08 -0700

    Refator so if suitcase closed with no selection input is cleared

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -371,7 +371,7 @@ class GiftedChat extends React.Component {
   renderInputToolbar() {
     const inputToolbarProps = {
       ...this.props,
-      text: this.state.text || this.props.text,
+      text: this.props.text === '###CLEAR###' ? '' : (this.props.text !== this.state.text && this.props.text !== '###CLEAR###' ? this.props.text : this.state.text),
       composerHeight: Math.max(MIN_COMPOSER_HEIGHT, this.state.composerHeight),
       onChange: this.onType,
       onSend: this.onSend,
